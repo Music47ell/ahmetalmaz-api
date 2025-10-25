fb7f4c3b6a1069854e0d4148ecfb94d246ac4396
Ahmet ALMAZ <github.loops6079@ahmetalmaz.com>
2025-10-25 21:01:36 +0300

    fix: add user-agent to trakt

--- src/trakt/stats.ts
+++ src/trakt/stats.ts
@@ -7,6 +7,7 @@ export const getTraktStats = async () => {
       'Content-Type': 'application/json',
       'trakt-api-version': '2',
       'trakt-api-key': TRAKT_CLIENT_ID,
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
     } as HeadersInit,
   })
 
--- src/trakt/watchedMovies.ts
+++ src/trakt/watchedMovies.ts
@@ -14,6 +14,7 @@ export const getWatchedMovies = async (): Promise<Movie[]> => {
       'Content-Type': 'application/json',
       'trakt-api-version': '2',
       'trakt-api-key': TRAKT_CLIENT_ID,
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
     } as HeadersInit,
   })
 
